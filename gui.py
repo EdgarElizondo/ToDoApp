459ef0e004366f8234399c222154d13ae84ddc3b
EdgarElizondo <edgar_7_23@hotmail.com>
2024-07-23 16:41:38 -0600

    1.- Add button implementation. 2.- Close window implementation

--- gui.py
+++ gui.py
@@ -2,10 +2,17 @@ import ToDoFunctions.ToDoFunctions as fn
 import FreeSimpleGUI as gui
 
 label = gui.Text("Type in a ToDo:")
-inputBox = gui.InputText(tooltip="Enter ToDo")
+inputBox = gui.InputText(tooltip="Enter ToDo", key="todo")
 addButton = gui.Button("Add")
 
 # Show Window
-window = gui.Window("My ToDo App", layout=[[label], [inputBox,addButton]])
-window.read()
+window = gui.Window("My ToDo App", 
+                    layout=[[label], [inputBox,addButton]],
+                    font=("Helvetica",10))
+while True:
+    event, values = window.read()
+    if event == "Add":
+        todos = fn.write_todos(values["todo"] + "\n", "a")
+    elif event is None:
+        break
 window.close()
